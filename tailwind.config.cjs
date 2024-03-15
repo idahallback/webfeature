c22147d6c54e24199d514e40cee30130b0ccf4d4
Ida Hallbæck <idha0001@stud.kea.dk>
2024-03-15 13:33:55 +0100

    c cursor

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    theme: {
+        extend: {
+          cursor: {
+            'handcursor': 'url(cursor.svg) 25 25 pointer',
+          }
+        }
+      },
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     plugins: [
         require('@tailwindcss/typography'),
